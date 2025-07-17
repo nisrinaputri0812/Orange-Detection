4ce7357440afdb92003126f5236340b55b481119
nisrinaputri0812 <nisrinaputri603@gmail.com>
2025-07-17 15:29:51 +0700

    Update about.py

--- about.py
+++ about.py
@@ -5,7 +5,7 @@ def run():
     st.write("Aplikasi ini dikembangkan untuk mendeteksi jeruk secara otomatis pada gambar atau video. Dengan memanfaatkan teknologi kecerdasan buatan dan computer vision, proses identifikasi jeruk menjadi lebih cepat, akurat, dan efisien.")
 
     st.header("ℹ️ Tentang YOLOv8")
-    st.write("""YOLOv8 (You Only Look Once versi 8) adalah model deteksi objek yang dikembangkan oleh Ultralytics. Model ini menggunakan pendekatan **real-time object detection** yang mampu mengenali objek dengan cepat dan akurat. YOLOv8 mendukung berbagai format input, termasuk gambar, video, serta kamera secara langsung, sehingga cocok untuk implementasi praktis di berbagai bidang.""")
+    st.write("""YOLOv8 (You Only Look Once versi 8) adalah model deteksi objek yang dikembangkan oleh Ultralytics. Model ini menggunakan pendekatan **real-time object detection** yang mampu mengenali objek dengan cepat dan akurat. YOLOv8 mendukung berbagai format input, termasuk gambar ataupun video sehingga cocok untuk implementasi praktis di berbagai bidang.""")
 
     st.header("ℹ️ Manfaat Aplikasi")
     st.write("""
